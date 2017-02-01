83c8fe2f62c4805fc94a55e477337def07d1df34
V.G.Elkin <elkinvg@gmail.com>
2017-02-01 12:45:55 +0100

    added termo png

--- app/view/lens/LensTemperature.js
+++ app/view/lens/LensTemperature.js
@@ -132,7 +132,68 @@ Ext.define('LensControl.view.lens.LensTemperature', {
                     handler: 'setMaximumTemperature'
                 }
             ]
-        }
+        },
+        {
+            xtype: 'panel',
+            width: 564,
+            height: 734,
+            layout: 'absolute',
+            bodyStyle: "background-image:url(resources/images/termo_new_png.png) !important",
+            defaultType: 'label'/*,
+            items: [
+                {
+                    reference: 'T_1',
+                    x: 26,
+                    y: 355,
+                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T1</span>'
+                },
+                {
+                    reference: 'T_2',
+                    x: 216,
+                    y: 245,
+                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T2</span>'
+                },
+                {
+                    reference: 'T_3',
+                    x: 304,
+                    y: 302,
+                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T3</span>'
+                },
+                {
+                    reference: 'T_4',
+                    x: 109,
+                    y: 186,
+                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T4</span>'
+                },
+                {
+                    reference: 'T_5',
+                    x: 214,
+                    y: 415,
+                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T5</span>'
+                },
+                                {
+                    reference: 'T_6',
+                    x: 514,
+                    y: 120,
+                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T6</span>'
+                },
+                {
+                    reference: 'T_7',
+                    x: 514,
+                    y: 216,
+                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T7</span>'
+                },
+                {
+                    x: 490, 
+                    y: 450,
+                    xtype: 'button',
+                    reference: 'graphbut',
+                    width: 150,
+                    text: 'Показать график',
+                    handler: 'getTemperatureChart'
+                }
+            ]*/
+        },
     ]
 });
 
