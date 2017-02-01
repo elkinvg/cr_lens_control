9b5232fd39ac759ce64a364c1bd368d8f3e44e0c
V.G.Elkin <elkinvg@gmail.com>
2017-02-01 13:19:36 +0100

    corrected positions

--- app/view/lens/LensTemperature.js
+++ app/view/lens/LensTemperature.js
@@ -139,32 +139,32 @@ Ext.define('LensControl.view.lens.LensTemperature', {
             height: 734,
             layout: 'absolute',
             bodyStyle: "background-image:url(resources/images/termo_new_png.png) !important",
-            defaultType: 'label'/*,
+            defaultType: 'label',
             items: [
                 {
-                    reference: 'T_1',
-                    x: 26,
-                    y: 355,
-                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T1</span>'
+                    reference: 'T2_1',
+                    x: 54,
+                    y: 395,
+                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T2_1</span>'
                 },
                 {
-                    reference: 'T_2',
+                    reference: 'T2_2',
                     x: 216,
-                    y: 245,
-                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T2</span>'
+                    y: 138,
+                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T2_2</span>'
                 },
                 {
-                    reference: 'T_3',
-                    x: 304,
-                    y: 302,
-                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T3</span>'
+                    reference: 'T2_3',
+                    x: 216,
+                    y: 242,
+                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T2_3</span>'
                 },
                 {
-                    reference: 'T_4',
-                    x: 109,
-                    y: 186,
-                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T4</span>'
-                },
+                    reference: 'T2_4',
+                    x: 380,
+                    y: 393,
+                    html: '<span style="font-weight:bold; color:blue; font-size:300%"> T2_4</span>'
+                }/*,
                 {
                     reference: 'T_5',
                     x: 214,
@@ -191,8 +191,8 @@ Ext.define('LensControl.view.lens.LensTemperature', {
                     width: 150,
                     text: 'Показать график',
                     handler: 'getTemperatureChart'
-                }
-            ]*/
+                }*/
+            ]
         },
     ]
 });
